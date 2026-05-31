438832c3ebf65198136f5d0d52058a2af095e0a0
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-31 22:33:46 +0300

    TagFeedScreen.js ekraninda constla tanimlandi.

--- src/screens/TagFeedScreen.js
+++ src/screens/TagFeedScreen.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+
+
+const TagFeedScreen = ({ navigation, route }) => {
+
+    const { tag } = route.params;
