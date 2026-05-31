45c593968b165e83eadb09d9cd3c54f0092a290d
Batın YILMAZ <batinylmz2323@gmail.com>
2026-05-31 22:34:47 +0300

    TagFeedScreen.js ekraninda return fonksiyonu olusturuldu.

--- src/screens/TagFeedScreen.js
+++ src/screens/TagFeedScreen.js
@@ -6,3 +6,20 @@ import { Ionicons } from '@expo/vector-icons';
 const TagFeedScreen = ({ navigation, route }) => {
 
     const { tag } = route.params;
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+                    <Ionicons name="chevron-back" size={28} color="#000" />
+                </TouchableOpacity>
+
+                <Text style={styles.headerTitle}>{tag} Tarifleri</Text>
+            </View>
+
+            <View style={styles.content}>
+                <Text>Burada "{tag}" etiketine ait yemek kartları listelenecek.</Text>
+            </View>
+        </View>
+    );
+};
